c58016b18a3fce41cd6bc98c3aad471bdf50d190
Dmitry Kulga <coolgadv@gmail.com>
2019-05-05 09:29:47 +0300

    Added more samples for using JUnit5.

--- junit5/src/main/java/cdv/libs/junit/SampleUtils.java
+++ junit5/src/main/java/cdv/libs/junit/SampleUtils.java
@@ -1,5 +1,6 @@
 package cdv.libs.junit;
 
+import lombok.Data;
 import lombok.experimental.UtilityClass;
 
 import java.util.Arrays;
@@ -9,6 +10,25 @@ public class SampleUtils {
 
     public int sum(int... numbers) {
         return Arrays.stream(numbers).sum();
+    }
+
+    public KeyValuePair sum(KeyValuePair first, KeyValuePair second) {
+        return new KeyValuePair(
+                first.getKey() + ":" + second.getKey(),
+                first.getValue() + "_" + second.getValue());
+    }
+
+    public String toUpperCase(String value) {
+        return value != null ? value.toUpperCase() : null;
+    }
+
+    @Data
+    public static class KeyValuePair {
+
+        private final String key;
+
+        private final String value;
+
     }
 
 }
--- junit5/src/test/java/cdv/libs/junit/SampleUtilsTest.java
+++ junit5/src/test/java/cdv/libs/junit/SampleUtilsTest.java
@@ -1,12 +1,36 @@
 package cdv.libs.junit;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static cdv.libs.junit.SampleUtils.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
+@TestInstance(PER_CLASS)
 class SampleUtilsTest {
+
+    @BeforeAll
+    void beforeAll() {
+        System.out.println(">>> before all");
+    }
+
+    @AfterAll
+    void afterAll() {
+        System.out.println(">>> after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println(">>> before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println(">>> after each");
+    }
 
     @Test
     @DisplayName("should sum two numbers")
@@ -18,6 +42,26 @@ class SampleUtilsTest {
     @DisplayName("should return single argument value")
     void sumSingleNumbers() {
         assertEquals(5, sum(5));
+    }
+
+    @Test
+    @DisplayName("should sum two key-value pairs")
+    void sumTwoKeyValuePairs() {
+
+        KeyValuePair first = new KeyValuePair("firstKey", "firstValue");
+        KeyValuePair second = new KeyValuePair("secondKey", "secondValue");
+
+        KeyValuePair sum = sum(first, second);
+
+        assertAll(
+                () ->  assertEquals("firstKey:secondKey", sum.getKey()),
+                () ->  assertEquals("firstValue_secondValue", sum.getValue()));
+    }
+
+    @ParameterizedTest(name = "should convert to upper case string: {0}")
+    @ValueSource(strings = { "test", "Test", "TeSt" })
+    void convertToUpperCase(String value) {
+        assertEquals("TEST", toUpperCase(value));
     }
 
 }
